1b03d2d65460516c6653b553c66efd789da65a5d
Jordan Abderrachid <jad@stripe.com>
2022-12-26 13:21:59 +0100

    complete day 3

--- 2022/day_3/run.py
+++ 2022/day_3/run.py
@@ -10,13 +10,13 @@ def item_in_common(lines):
     first_group = list(lines[0])
     second_group = list(lines[1])
     third_group = list(lines[2])
-    half_size = int(len(second_group)/2)
-    first_half, second_half = first_group + second_group[0:half_size], second_group[half_size:] + third_group
-    return (set(first_half) & set(second_half)).pop()
+    return (set(first_group) & set(second_group) & set(third_group)).pop()
 
 def run():
     f = read_input("day_3")
-    print(item_in_common(["vJrwpWtwJgWrhcsFMMfFFhFp", "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL", "PmmdzqPrVvPwwTWBwg"]))
-    # lines = f.split("\n")
-    # priorities = [priority(item_in_common(line)) for line in lines]
-    # print(reduce(lambda x,y:x+y, priorities, 0))
+    # print(item_in_common(["vJrwpWtwJgWrhcsFMMfFFhFp", "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL", "PmmdzqPrVvPwwTWBwg"]))
+    lines = f.split("\n")
+    priorities = []
+    for i in range(0, int(len(lines)/3)):
+        priorities.append(priority(item_in_common(lines[3*i:3*i+3])))
+    print(reduce(lambda x,y:x+y, priorities, 0))
